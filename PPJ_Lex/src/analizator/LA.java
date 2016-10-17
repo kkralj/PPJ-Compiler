e4c76bac0a9ee4b9ab50f107a1fdcd2b9858e5c8
= <=>
2016-10-18 01:16:39 +0200

    Added LA and started ENKA

---
+++ PPJ_Lex/src/analizator/LA.java
@@ -0,0 +1,21 @@
+package analizator;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.util.Scanner;
+
+public class LA {
+	private static String data;
+	private static String state;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		while ((line = bi.readLine()) != null) {
+			data += line;
+		}
+	}
+}
